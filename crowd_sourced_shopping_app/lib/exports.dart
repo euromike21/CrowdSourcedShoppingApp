eb56d1221045fff46e984eef53bff6a059358548
Joshpnguyen <Joshpnguyen@gmail.com>
2021-10-12 20:09:57 -0700

    Initial commit
    Initialized Dart project files.

---
+++ crowd_sourced_shopping_app/lib/exports.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+export 'screens/home_screen.dart';
+export 'screens/profile_screen.dart';
+export 'screens/shopping_list_screen.dart';
---
+++ crowd_sourced_shopping_app/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:crowd_sourced_shopping_app/exports.dart';
+
+void main() {
+  runApp(MyApp());
+}
