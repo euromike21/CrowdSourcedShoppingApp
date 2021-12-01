0bc6a3ba926ea38d45ae01302205b385ef857e33
Mihai Ion <ionm@oregonstate.edu>
2021-11-30 22:54:02 -0800

    updated to get permission on front page

--- crowd_sourced_shopping_app/lib/screens/home_page.dart
+++ crowd_sourced_shopping_app/lib/screens/home_page.dart
@@ -13,8 +13,6 @@ class _MyHomePageState extends State<MyHomePage> {
   UserModel loggedInUser = UserModel();
   late UserProf usr = UserPreferences.getUser();
   int selected_index = 0;
-  var latitudeData = "";
-  var longitudeData = "";
 
   @override
   void initState() {
@@ -32,13 +30,41 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future _determinePosition() async {
+  Future<Position> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
     permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 
   // Tabs for navigation bar
